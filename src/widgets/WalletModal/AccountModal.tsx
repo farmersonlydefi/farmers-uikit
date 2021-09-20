6d3a02c4b09e3c4a90d8fc9fbda68e72937e40db
hammon4tl <91074469+farmersonlydefi@users.noreply.github.com>
2021-09-20 19:42:41 +0200

    init

--- src/widgets/WalletModal/AccountModal.tsx
+++ src/widgets/WalletModal/AccountModal.tsx
@@ -23,8 +23,8 @@ const AccountModal: React.FC<Props> = ({ account, logout, onDismiss = () => null
       {account}
     </Text>
     <Flex mb="32px">
-    <LinkExternal small href={`https://explorer-mainnet.maticvigil.com/address/${account}`} mr="16px">
-        View on Matic Explorer
+    <LinkExternal small href={`https://cchain.explorer.avax.network/address/${account}`} mr="16px">
+        View on Avax Explorer
       </LinkExternal>
       <CopyToClipboard toCopy={account}>Copy Address</CopyToClipboard>
     </Flex>
