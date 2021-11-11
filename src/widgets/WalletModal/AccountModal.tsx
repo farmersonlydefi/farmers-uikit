a24e2b9c5764c8901c7f6e8d543198b2bb64545c
FarmersOnly Finance <dev@farmersonly.farm>
2021-11-11 01:31:35 +0100

    init

--- src/widgets/WalletModal/AccountModal.tsx
+++ src/widgets/WalletModal/AccountModal.tsx
@@ -23,8 +23,8 @@ const AccountModal: React.FC<Props> = ({ account, logout, onDismiss = () => null
       {account}
     </Text>
     <Flex mb="32px">
-    <LinkExternal small href={`https://cchain.explorer.avax.network/address/${account}`} mr="16px">
-        View on Avax Explorer
+    <LinkExternal small href={`https://snowtrace.io/address/${account}`} mr="16px">
+        View on the Snowtrace
       </LinkExternal>
       <CopyToClipboard toCopy={account}>Copy Address</CopyToClipboard>
     </Flex>
